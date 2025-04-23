d29171664380b446931c09e907511df05170f786
iamshivamgaur <shivam24august@gmail.com>
2025-04-24 00:28:11 +0530

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -25,6 +25,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions))
 app.use(cookieParser());
 app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ limit: "30kb", extended: true }));
