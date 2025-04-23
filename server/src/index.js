2663203bfcc95bed7b909b2c97c810ed84f31663
iamshivamgaur <shivam24august@gmail.com>
2025-04-24 01:03:56 +0530

    adding

--- server/src/index.js
+++ server/src/index.js
@@ -29,7 +29,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 // This line specifically handles all OPTIONS requests
-app.options("*", cors(corsOptions)); 
+app.options("/*", cors(corsOptions)); 
 app.use(cookieParser());
 app.use(express.json({ limit: "30kb" }));
 app.use(express.urlencoded({ limit: "30kb", extended: true }));
