491f8470858cd7259cf1df11aa329ac0e1b9f596
ishivamgaur <shivam24august@gmail.com>
2026-08-30 17:00:34 +0530

    fix(db): add DNS override to bypass ISP blocking of MongoDB SRV records

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Force Node.js to use Google DNS to bypass local ISP/Wi-Fi DNS blocking MongoDB SRV queries
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const ConnectDb = async () => {
   try {
